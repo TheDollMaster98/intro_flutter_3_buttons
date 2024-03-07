5f5f2acad9b52f7c06f252350a4affafe69d5b3d
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-03-07 13:49:08 +0100

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:intro_flutter_3_buttons/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: HomePage(),
+    );
+  }
+}
