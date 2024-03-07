200a579a1bdbeb6d41f1a0680b3e74a4139d75d0
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-03-07 16:20:10 +0100

    added tutorial for buttons

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:intro_flutter_3_buttons/pages/home_page.dart';
 
+// La classe App estende StatelessWidget, che è la base per i widget che descrivono una parte dell'interfaccia utente in termini di altri widget più bassi.
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // MaterialApp è il widget radice per la maggior parte delle app Flutter, e include una serie di funzionalità importanti come la navigazione e i temi.
     return MaterialApp(
-        home: HomePage(),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,59 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  // Questo è un metodo che viene chiamato quando il pulsante viene premuto.
+  void onButtonPressed() {
+    print("Pressed");
   }
 
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp è il widget radice per la maggior parte delle app Flutter, e include una serie di funzionalità importanti come la navigazione e i temi.
+    return MaterialApp(
+      home: Scaffold(
+        appBar: appBar(),
+        // Center è un widget che centra il suo figlio nel se stesso.
+        // Qui, il figlio è il metodo body() che restituisce un widget Column.
+        body: Center(child: body()),
+      ),
+    );
+  }
+
+  // Questo metodo restituisce un AppBar, che è una barra di applicazioni tipica per le applicazioni Material Design.
+  AppBar appBar() => AppBar(
+    title: Text("Button"),
+    backgroundColor: Colors.purple,
+  );
+
+  // Questo metodo restituisce un widget Column, che prende una lista di figli e li dispone verticalmente.
+  // In questo caso, i figli sono un TextButton, un ElevatedButton e un IconButton.
+  Widget body() => Column(
+    // MainAxisAlignment.center posiziona i figli il più vicino possibile al centro lungo l'asse verticale.
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      // TextButton è un tipo di pulsante che mostra il testo che descrive l'azione che verrà eseguita quando il pulsante viene premuto.
+      TextButton(
+        onPressed: onButtonPressed,
+        child: Text("Text Button",
+          style: TextStyle(
+              fontSize: 30
+          ),
+        ),
+      ),
+      // ElevatedButton è un tipo di pulsante Material Design che contiene inchiostro splash e ombreggiatura.
+      ElevatedButton(
+        onPressed: onButtonPressed,
+        child: Text("Elevated Button",
+          style: TextStyle(
+              fontSize: 30
+          ),
+        ),
+      ),
+      // IconButton è un tipo di pulsante che contiene un'icona senza testo che descrive l'azione che verrà eseguita quando il pulsante viene premuto.
+      IconButton(
+        onPressed: onButtonPressed,
+        icon: Icon(Icons.plus_one, size: 50, color: Colors.pink),
+      )
+    ],
+  );
 }
